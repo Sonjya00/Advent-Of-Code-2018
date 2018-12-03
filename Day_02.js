08a8d0aff81af489007b42bd31ef57fea8eea708
Sonia Gorla <soniagorla@msn.com>
2018-12-03 03:40:33 -0600

    Add Day_02

---
+++ Day_02.js
@@ -0,0 +1,339 @@
+const boxIDs = `mphcuiszrnjzxwkbgdzqeoyxfa
+mihcuisgrnjzxwkbgdtqeoylia
+mphauisvrnjgxwkbgdtqeiylfa
+mphcuisnrnjzxwkbgdgqeoylua
+mphcuisurnjzxwkbgdtqeoilfi
+mkhcuisvrnjzowkbgdteeoylfa
+mphcoicvrnjzxwksgdtqeoylfa
+mxhcuisvrndzxwkbgdtqeeylfa
+dphcuisijnjzxwkbgdtqeoylfa
+mihvuisvrqjzxwkbgdtqeoylfa
+mphcuisrrnvzxwkbgdtqeodlfa
+mphtuisdrnjzxskbgdtqeoylfa
+mphcutmvsnjzxwkbgdtqeoylfa
+mphcunsvrnjzswkggdtqeoylfa
+mphcuisvrwjzxwkbpdtqeoylfr
+mphcujsdrnjzxwkbgdtqeovlfa
+mpfcuisvrdjzxwkbgdtteoylfa
+mppcuisvrpjzxwkbgdtqeoywfa
+mphcuisvrnjzxwkbfptqroylfa
+mphcuisvrnjzxwkbgstoeoysfa
+mphcufsvrnjzcwkbgdeqeoylfa
+mphcuissrnjzxwkbgdkquoylfa
+sphcuxsvrnjzxwkbgdtqioylfa
+mphcuiivrhjzxwkbgdtqevylfa
+echcuisvrnjzxwkbgltqeoylfa
+mphcuisvrljexwkbvdtqeoylfa
+mpjcuisvrnjzxwkhidtqeoylfa
+mphcuisvrfjzmwkbgdtqeoylfl
+mwhcuisvrnjzxwkbgdtqeoytfm
+mphcuisvrsjzxwkbgdaqeoylfh
+mohcuisvrnjzxwkbgdtqtoymfa
+maycuisvrnjzxwkbgdtqboylfa
+pphcuisvqnjzxwkbgdtqeoylfd
+mprcuisvrnjtxwmbgdtqeoylfa
+mfhcuisgrnjzxckbgdtqeoylfa
+mphiubsvrnjzxwkbgdtqeoyufa
+dphctisvrnjzxwkbgdtqeoylfk
+mphcuisvrnjznwksgdtqeoyzfa
+mpwcuisvrnjziwkbgdtqaoylfa
+mphduzsvrnjznwkbgdtqeoylfa
+mphccisvrnjzxwebgdtqeoylqa
+xphcuisvrnjzxwkfvdtqeoylfa
+mphcupsvrnjzxwkbgdtfeoylpa
+mphcuisvrtjzjwkbgdtqeoylfe
+mpbcuisvrnjzxwkbgdmieoylfa
+mphcuisvrnjzxwkbgjtqetylaa
+mphcuisvrnjzxwpbgdtgdoylfa
+ophcufsvrqjzxwkbgdtqeoylfa
+iphcuhsvrnjzxwkbgetqeoylfa
+mphcuisvunjzxwwbgdtqeoylqa
+mphcpisvrnjzowkbgdtveoylfa
+mphcuisvrnjzxhkbgdtqeotlla
+mphcuisvrnjzxwkbodtgeoylha
+mphcuisvrjjzxwkbwdtqtoylfa
+mphcwisvrnjnxwkbgjtqeoylfa
+mplcuicqrnjzxwkbgdtqeoylfa
+mphcuisvrnjzxydbgdtqeoylfn
+ophckisvrnjzxwkbgdtqeozlfa
+mphcuisvrkjzxwkbgdtteoblfa
+yphcuisvrnjcxwkbggtqeoylfa
+mphcuisvrnazxwfbqdtqeoylfa
+mphcuisvrmjzxwkbgdtlwoylfa
+mphctksvrnjzxwibgdtqeoylfa
+mphcuisprnjzxlebgdtqeoylfa
+mphcuisnrnjzxakbgdtueoylfa
+mphcuiavrnjoxwtbgdtqeoylfa
+nphcuisvrnjzxwkbgdtqzoylfk
+mphcuisrrnjmxwkbgdtqdoylfa
+mphcuisvrujzxwkvgdtqehylfa
+mphcuisvrnfzxwkogdtqebylfa
+mphcuisvrnjwdwkbgdtqeoyxfa
+mphcuisvrntzxwkrgxtqeoylfa
+mpzcuisvrnjzxwebgdtqeoylsa
+aphcuikvrnjzxwwbgdtqeoylfa
+mphcqisvrnjzxwkpgdtqeoelfa
+mphcuusvrnjzxwkbgdtjeodlfa
+mphcuisvrnjzewkbgdtteoylza
+mphcuisvanjzxwkbgdtheoylfc
+mphcjishrnjzxwkbgltqeoylfa
+mpxcuislrnjzxwkbgdtqeoynfa
+mphcuisvrnjjxwkbgdtmeoxlfa
+mphcimsvrnjzxwkbsdtqeoylfa
+mphcxisvcnjzxwjbgdtqeoylfa
+mphcuisbrvjzxwkbgdtqeoymfa
+mplcuisvrnjzxwkbgdtaenylfa
+mphcuihvrnjzxwkygytqeoylfa
+mphcbisvrnjzxhkbgdtqezylfa
+mphcuisarnjzxwkbgatqeoylfv
+mphcumsvrnjzxwkbgdrqebylfa
+mlhcuisvrnwzxwkbgdtqeoylfx
+mpkcuisvrkjzxwkbgdtqeoylfo
+mphcuissrnjzxwkbgdtqmoylfc
+mphcuiwvrnjuxwkfgdtqeoylfa
+mphcuicvlnjzxwkbgdvqeoylfa
+mphcuisvrvvzxwkbfdtqeoylfa
+myhcuisvrnjpxwkbgntqeoylfa
+mpocuisvrnjzxwtbgitqeoylfa
+mphcuisvrnjzxwkbgdtwewyqfa
+mphcuisvtnjzxwwbgdtqeoolfa
+mphcuisvrnjzxgkbgdyqeoyyfa
+mphcuisvrdjzxwkbgpyqeoylfa
+bphcuisvrnjzxwkbgxtqefylfa
+sphcuisvrdjzxwktgdtqeoylfa
+mphcuvsvrnjmxwobgdtqeoylfa
+mphcuisvrnjzxwkbsdtqeuylfb
+mnhcmisvynjzxwkbgdtqeoylfa
+mphckisvrnjzxwkhgdkqeoylfa
+mpacuisvrnjzxwkbgdtqeoolaa
+mpgcuisvrnjzxwkbzdtqeoynfa
+mphcuisvrojzxwkbzdtqeoylga
+mphcuisvknjfxwkbydtqeoylfa
+mphcuistrnjzxwkbgdqqeuylfa
+bpvcuiszrnjzxwkbgdtqeoylfa
+mphcuxsvrnjzswkbgdtqeoelfa
+mphcuisvbnjzxwlbgdtqeoylla
+mphcuisvonczxwkbgktqeoylfa
+mphcuisvrnkzxwvbgdtquoylfa
+mphcuisvrnjzxokfgdtqeoylia
+tphcuisvrnjzxwkbjdwqeoylfa
+mihcuisvrnjzpwibgdtqeoylfa
+mphcuisvrejzxwkbgdtqjuylfa
+mprcuisvrnjixwkxgdtqeoylfa
+mpqcuiszrnjzxwkbgdtqeodlfa
+mphcuasvrnjzzakbgdtqeoylva
+mphcuisvrnjzmwkbtdtqeoycfa
+mphcuisvrnjzxwkbcdtqioylxa
+mphckisvrnjzxwkbcdtqeoylfm
+mphcuisvrnjuxwbogdtqeoylfa
+mphcuisdrnjzxwkbldtqeoylfx
+mphcuisvrnjoxwkbgdtqeyyyfa
+mphcuicvqnjzxwkbgdtqeoylna
+mpmcuisvrnjzxwkbgdtqktylfa
+mphcuisvrnqzxwkggdtqeoykfa
+mphcuisvryjzxwkbydtqejylfa
+mphcugsvrnjzxwkbghtqeeylfa
+rphcuusvrnjzxwkwgdtqeoylfa
+zphwuiyvrnjzxwkbgdtqeoylfa
+cphcuivvrnjzxwkbgdtqenylfa
+mphcuisvrnjzxwkagotqevylfa
+mprcuisvrcjzxwkbgdtqeoytfa
+mphjugsvrnezxwkbgdtqeoylfa
+mphcuisvryjzxwkbgltqeoylaa
+mphcursvrnjzxfkbgdtqeoydfa
+mphcuisvrcuzxwkbgdtqeoylfw
+mphcuisvrijzxwkbgdtqeoelfh
+xphcuisvenjzxjkbgdtqeoylfa
+mphcuisvrnazxwkbgdeqeoylaa
+mphcuisbrsjzxwkbgdtqeoygfa
+mlhvuisvrnjzxwkbgdtqeoylfh
+mphcuisvrnjzxukbgdtqeoyhfy
+mpzcuilvrnjzawkbgdtqeoylfa
+hphcuisjfnjzxwkbgdtqeoylfa
+mahcuisvrnjzxwkegdtqeoylfi
+mphcuixvrnjzcwkbgdtqetylfa
+mphcuisvrnjzxwkdgdtqeoklfj
+mlhcuisvrnjzxwkbgdteeoylka
+mphcuifvrnjbxwkrgdtqeoylfa
+mphcuasvrnjzzwkbgdtqeoylva
+mphcuisvrnjzxwkboutqeoylba
+mbhcuisvcnjzxwklgdtqeoylfa
+mpbcuisvrnjzxgkbgdtqesylfa
+mphcuisvrnjfswkbgdtqeoylfd
+mphcuisvrnjzxwkbgdoweoysfa
+uphcuisvrnjzrwkbgdtqelylfa
+mphcuisvrnjzxwkbgdtqyoylsi
+mpqcuiqvxnjzxwkbgdtqeoylfa
+mphcuisorfjzxwkbgatqeoylfa
+mphcuisvrntfxwkbzdtqeoylfa
+mphcuisvrnrzxwkbgdtueoylfl
+mphcuisvrnjzewkagdtyeoylfa
+mpocuisdrnjzxwkbgdtqeozlfa
+mphcuisvrnjjxwkbgdtoeoylfm
+mphcuisvenjzxwkbgdtqwoylza
+mpmcuisvrnjzxwkbgdtqeoxlfr
+mphcuisvgnjhxwkbgdtqeoplfa
+mphcuisvrnjzowkdgdtqeoyyfa
+mphcuisqynjzxwkbgdtqeoylda
+hphcuisvgnjzxwkbgdtbeoylfa
+iphcuipvrnuzxwkbgdtqeoylfa
+mphcuisvrnjzsikbpdtqeoylfa
+mpwcuhsvrnjzxbkbgdtqeoylfa
+mnhjuisvcnjzxwkbgdtqeoylfa
+mphcudsvrnjzxwkbgdtqloilfa
+mpncuiwvrwjzxwkbgdtqeoylfa
+mphcuisvrnjgawkbgdtqeoylya
+mphcuisvrnjzxwkbggtteoslfa
+mphcuisvrnjzxwkbgdvqeoylpe
+mphcuisvrnczxfkbgktqeoylfa
+mphcuifvrnjzxwkbgdbmeoylfa
+mphcuisvrnjytwkbgdtqeoylla
+mphcuisvrnjzxwkbgdtjeoxlfn
+mphjuisvrnjzxwkbghtqeoyffa
+mphcuisvrnjzxkrbgdtqeoylaa
+mphcbisvrnjzxwkbgttqeoylfs
+mphkuksvbnjzxwkbgdtqeoylfa
+nphcuidvrnjzxwhbgdtqeoylfa
+mphguzsvrnjzxwkbgdaqeoylfa
+mihcuisfrnjzxwkbgdtqhoylfa
+mphcuisvrnrzxwpbgdtqesylfa
+zphcuisvrnjzxwkbddtqeoylaa
+mphcuigvmnjzxwkbgdtqeoylba
+mjhcuisvrnjzxjkbgdtqeoylha
+mphnuisvrnjznwkbgdtqnoylfa
+mkhcuisvrnjcxwkbgdqqeoylfa
+mphcuisvenjzxwbbqdtqeoylfa
+qphcuisnrnjzawkbgdtqeoylfa
+mphcuisvrdjzxwkbgdtqeoywca
+mphcuzsvvnjzxwfbgdtqeoylfa
+pphcuxsvrnjzxwkbgdtmeoylfa
+mphiuvsvrnjzxlkbgdtqeoylfa
+mphlqisvrnjzxkkbgdtqeoylfa
+mmhcuisvrnjzxwkbgatqeoylea
+mphduisrrnjoxwkbgdtqeoylfa
+mphcuisvrnjnxwkvgdyqeoylfa
+mphcuvsvrnjzxgkbgdtqeoylfz
+mphcuisvryjzxwkbggtqkoylfa
+iphcuisvrdjzxwkbgotqeoylfa
+mphcuisvrnjzxwhbgdtqwoyofa
+mphcorbvrnjzxwkbgdtqeoylfa
+mghcuisvrnpzxykbgdtqeoylfa
+mphauisvrnjnxwkbzdtqeoylfa
+mphcgisvrnjzxwkwgdtqeoygfa
+mphcuisvrnjzxwkggotqeoylba
+mphcuesvrnjzxwkbgdwqebylfa
+yphcuisvrnjzxwkbgdxqeoylja
+ephyuisvrnjzywkbgdtqeoylfa
+mfhcuisqrnjzxwkbgdlqeoylfa
+mphkuisvrnjzxwkbertqeoylfa
+mphcuusgrnjzxwkbggtqeoylfa
+mphcuildrnjvxwkbgdtqeoylfa
+mphcuiuvrnjzlwkbgwtqeoylfa
+mppcuisvrljzxwkbgdtqeoylfw
+mphcwiwvrnjzxwsbgdtqeoylfa
+mphcubivrnjzxwkqgdtqeoylfa
+mphcuisvrnjpxwkngdtqeoylpa
+pchcuisvrgjzxwkbgdtqeoylfa
+mphcuisvlnjzxwkbgdtmeoylfw
+mphcuisvrnjzywkbgdvqeoylfj
+mpzcuisvrnezxwktgdtqeoylfa
+mphcuisvrnjbxwkbgzrqeoylfa
+mphcuisvrnjzxwktgdtqeodtfa
+jphcuiavrnjzxwkbgdtqeoylfv
+mphcuisvrnjzxwkbddppeoylfa
+mphcuissrkjzxwkbgxtqeoylfa
+mphcuisvrhjzxwxbgdtqeoylxa
+mphcvisvgnjjxwkbgdtqeoylfa
+mphcuisprnjwxwtbgdtqeoylfa
+mphcuissrnjzxqkbgdtqeoymfa
+mphcuiabrnjzxokbgdtqeoylfa
+mphcuisvrnczxwkbgmtpeoylfa`;
+
+const boxIDsArray = boxIDs.split("\n");
+
+// FIRST CHALLENGE
+function matchingPatterns(arr) {
+  // Regexp that matches 2 or 3 consecutive charaters
+  const regex = /(\w)\1{1,2}/g;
+
+  const matchesFound = [];
+  let twoRepeatingLetters = 0;
+  let threeRepeatingLetters = 0;
+
+  // Loop over the array
+  arr.forEach(value => {
+    // Sort each string to make it easier to find matches
+    const sortedString = value
+      .split("")
+      .sort()
+      .join("");
+    // if either 2 or 3 character matches, return an array with all matches
+    let result = sortedString.match(regex);
+    // if matches are found
+    if (result) {
+      // if there is more than 1 match
+      if (result.length > 1) {
+        // return an array containing either the match with longest length,
+        // or both matches if they are of different length;
+        result = result.reduce((a, b) => {
+          return a.length !== b.length
+            ? [a, b]
+            : a.length > b.length
+            ? [a]
+            : [b];
+        });
+      }
+      // add the value(s) to the array of matches found
+      result.forEach(value => matchesFound.push(value));
+    }
+  });
+
+  // Count how many matches contain 2 or 3 letters;
+  matchesFound.forEach(match => {
+    if (match.length === 2) {
+      twoRepeatingLetters++;
+    } else if (match.length === 3) {
+      threeRepeatingLetters++;
+    }
+  });
+
+  // Multiply the two results
+  const multiply = twoRepeatingLetters * threeRepeatingLetters;
+  return multiply;
+}
+matchingPatterns(boxIDsArray);
+
+// SECOND CHALLENGE
+
+// Find out which string have all letters in common but one
+function findMatchingLetters(arr) {
+  // variable to list all matching letters in the words compared
+  let matchingLetters = "";
+  // Loop to get the index of the first word to compare
+  for (let indexWordA = 0; indexWordA < arr.length; indexWordA++) {
+    // Loop to get the index of the second word to compare
+    for (
+      let indexWordB = indexWordA + 1;
+      indexWordB < arr.length;
+      indexWordB++
+    ) {
+      // Loop over the letters of both words
+      for (let char = 0; char < arr[0].length; char++) {
+        let currentWord = arr[indexWordA];
+        let comparingWord = arr[indexWordB];
+        // if the letter matches, add it to matchingLetters
+        if (currentWord[char] === comparingWord[char]) {
+          matchingLetters += currentWord[char];
+        }
+      }
+      // check if all the letters match except for one
+      if (matchingLetters.length === arr[indexWordA].length - 1) {
+        return matchingLetters;
+      } else {
+        // if not, reset the matchingLetters value and restart the loop
+        matchingLetters = "";
+      }
+    }
+  }
+}
+findMatchingLetters(boxIDsArray);
